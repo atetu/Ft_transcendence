24eed0dc6cde234a5d044ced3ecc9f657e080213
Caceresenzo <caceresenzo1502@gmail.com>
2021-05-12 20:30:29 +0200

    feat(backend/marvin): Finished verify code

--- backend/src/security/oauth.ts
+++ backend/src/security/oauth.ts
@@ -3,12 +3,50 @@ import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
 
 import { User } from "../entities/User";
 
-import { Strategy as GoogleStrategy } from "passport-google-oauth2";
+import {
+  Strategy as GoogleStrategy,
+  VerifyFunction,
+} from "passport-google-oauth2";
 import { Strategy as MarvinStrategy } from "../auth/marvin";
 
 import { env } from "../app";
 import { assert } from "console";
 import { authenticate } from "../services/UserService";
+
+const verify: VerifyFunction = async (
+  accessToken,
+  refreshToken,
+  profile,
+  callback
+) => {
+  try {
+    const userRepository = getRepository(User);
+
+    const email = profile.email;
+    assert(email, "email value is null");
+
+    let user = await userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return callback(null, await authenticate(user));
+    }
+
+    user = new User();
+    user.username = email.split("@")[0];
+    user.email = email;
+    user.admin = false;
+
+    await userRepository.save(user);
+
+    callback(null, await authenticate(user));
+  } catch (error) {
+    callback(error, null);
+  }
+};
 
 export function install() {
   passport.use(
@@ -18,35 +56,7 @@ export function install() {
         clientSecret: env.GOOGLE_SECRET,
         callbackURL: "http://localhost:3000/auth/google/callback",
       },
-      async function (accessToken, refreshToken, profile, callback) {
-        try {
-          const userRepository = getRepository(User);
-
-          const email = profile.email;
-          assert(email, "email value is null");
-
-          let user = await userRepository.findOne({
-            where: {
-              email,
-            },
-          });
-
-          if (user) {
-            return callback(null, await authenticate(user));
-          }
-
-          user = new User();
-          user.username = email.split("@")[0];
-          user.email = email;
-          user.admin = false;
-
-          await userRepository.save(user);
-
-          callback(null, await authenticate(user));
-        } catch (error) {
-          callback(error, null);
-        }
-      }
+      verify
     )
   );
 
@@ -58,15 +68,7 @@ export function install() {
         clientSecret: env.MARVIN_SECRET,
         callbackURL: "http://localhost:3000/auth/marvin/callback",
       },
-      function (accessToken, refreshToken, profile, callback) {
-        console.log(accessToken);
-        console.log(refreshToken);
-        console.log(profile);
-
-        callback(null, {
-          email: profile.email,
-        });
-      }
+      verify
     )
   );
 }
