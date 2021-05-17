1eb1be2ca93cb71f31c66aff46ac63537d723e5f
Caceresenzo <caceresenzo1502@gmail.com>
2021-05-18 00:25:13 +0200

    chore(back/auth): Reorganized Users API route directories

---
+++ backend/src/api/routes/users/index.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+import Container from "typedi";
+import UserService from "../../../services/UserService";
+import middlewares from "../../middlewares";
+import me from "./@me";
+
+export default (app: express.Router) => {
+  const userService = Container.get(UserService);
+
+  const route = express.Router();
+
+  app.use("/users", middlewares.authorize(false), route);
+
+  route.get("/", async (req, res, next) => {
+    res.status(200).send(await userService.all());
+  });
+
+  me(route);
+
+  return route;
+};
