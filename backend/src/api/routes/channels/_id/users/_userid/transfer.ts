cfda8159c8c5e7d7a280fa155de748cc20c0c603
Caceresenzo <caceresenzo1502@gmail.com>
2021-06-03 11:54:53 +0200

    feat(back/channel): Returning 403 when a user try to transfer ownership of it is not an admin

--- backend/src/api/routes/channels/_id/users/_userid/transfer.ts
+++ backend/src/api/routes/channels/_id/users/_userid/transfer.ts
@@ -2,7 +2,9 @@ import * as express from "express";
 import { Container } from "typeorm-typedi-extensions";
 import Channel from "../../../../../../entities/Channel";
 import ChannelUser from "../../../../../../entities/ChannelUser";
+import User from "../../../../../../entities/User";
 import ChannelService from "../../../../../../services/ChannelService";
+import helpers from "../../../../../helpers";
 
 export default (app: express.Router) => {
   const channelService = Container.get(ChannelService);
@@ -12,8 +14,13 @@ export default (app: express.Router) => {
   app.use("/transfer", route);
 
   route.post("/", async (req, res, next) => {
+    const user: User = req.user as any;
     const channel: Channel = res.locals.channel;
     const channelUser: ChannelUser = res.locals.channelUser;
+
+    if (user.id !== channel.owner.id) {
+      return helpers.forbidden("only owner can transfer ownership");
+    }
 
     await channelService.transferOwnership(channel, channelUser);
 
