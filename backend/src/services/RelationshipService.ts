d34d630ef5285cc53e98196f1aa6c0751fed7957
bmellouk <badriamellouk@gmail.com>
2021-06-29 15:19:15 +0200

    feat(back/relationship): added endpoint to check statut between me and a peer

--- backend/src/services/RelationshipService.ts
+++ backend/src/services/RelationshipService.ts
@@ -1,5 +1,9 @@
 import { Service } from "typedi";
 import { InjectRepository } from "typeorm-typedi-extensions";
+import relationships from "../api/routes/users/me/relationships";
+import Relationship, {
+  Type as RelationshipType,
+} from "../entities/Relationship";
 import User from "../entities/User";
 import RelationshipRepository from "../repositories/RelationshipRepository";
 
@@ -11,6 +15,60 @@ export default class RelationshipService {
   ) {}
 
   async all(user: User) {
-    return this.repository.findAllByUser(user)
+    return this.repository.findAllByUser(user);
+  }
+
+  async findRelationship(user: User, peer: User) {
+    return await this.repository.findByUserAndPeer(user, peer);
+  }
+
+  async findRelationships(user: User, peer: User) {
+    const a = await this.repository.findByUserAndPeer(user, peer);
+    const b = await this.repository.findByUserAndPeer(peer, user);
+
+    return [a, b];
+  }
+
+  async accept(a: Relationship, b: Relationship) {
+    a.type = RelationshipType.FRIEND;
+    b.type = RelationshipType.FRIEND;
+
+    await this.repository.save(a);
+    await this.repository.save(b);
+  }
+
+  async block(a: Relationship, b: Relationship) {
+    a.type = RelationshipType.BLOCK;
+
+    await this.repository.save(a);
+    await this.repository.delete(b);
+  }
+
+  async ask(user: User, peer: User) {
+    const a = new Relationship();
+    a.user = user;
+    a.peer = peer;
+    a.type = RelationshipType.OUTCOMING;
+
+    const b = new Relationship();
+    b.user = peer;
+    b.peer = user;
+    b.type = RelationshipType.INCOMING;
+
+    await this.repository.save(a);
+    await this.repository.save(b);
+
+    return [a, b];
+  }
+
+  async blockUser(user: User, peer: User) {
+    const a = new Relationship();
+    a.user = user;
+    a.peer = peer;
+    a.type = RelationshipType.BLOCK;
+
+    await this.repository.save(a);
+
+    return a;
   }
 }
