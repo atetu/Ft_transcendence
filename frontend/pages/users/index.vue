898fe760396221ff52ba9dfeecbb71a621a63bf6
Caceresenzo <caceresenzo1502@gmail.com>
2021-07-17 12:42:19 +0200

    feat(front/user): added a search bar

--- frontend/pages/users/index.vue
+++ frontend/pages/users/index.vue
@@ -6,6 +6,17 @@
           <v-card-title>
             Users
             <v-spacer />
+            <v-text-field
+              v-model="filter"
+              clearable
+              solo
+              outlined
+              dense
+              hide-details
+              prepend-inner-icon="mdi-magnify"
+              style="max-width: 200px"
+              class="mr-4"
+            />
             <v-btn icon :loading="$fetchState.pending" @click="$fetch">
               <v-icon>mdi-refresh</v-icon>
             </v-btn>
@@ -15,7 +26,7 @@
               Could not fetch users: {{ $fetchState.error }}
             </v-alert>
           </v-card-text>
-          <user-list :users="users" />
+          <user-list :users="users" :filter="filter" />
         </v-card>
       </v-col>
     </v-row>
@@ -29,6 +40,7 @@ import { User } from '~/models'
 @Component
 export default class Page extends Vue {
   users: User[] = []
+  filter = ''
 
   async fetch() {
     this.users = await this.$axios.$get('/users')
