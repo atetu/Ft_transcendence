ba71a22b7b100429fd20956389052912e07b4d92
Caceresenzo <caceresenzo1502@gmail.com>
2021-07-24 12:15:28 +0200

    fix(front/game): improved challenge dialog

--- frontend/components/direct-message/button/Challenge.vue
+++ frontend/components/direct-message/button/Challenge.vue
@@ -1,43 +1,37 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="600px">
+  <v-dialog v-model="dialog" max-width="600px">
     <template #activator="{ on, attrs }">
       <v-btn v-if="small" icon color="primary" v-bind="attrs" v-on="on">
         <v-icon>mdi-sword-cross</v-icon>
       </v-btn>
-      <v-btn
-        v-else
-        color="primary"
-        v-bind="attrs"
-        v-on="on"
-        @click="dialog = true"
-      >
+      <v-btn v-else color="primary" :block="block" v-bind="attrs" v-on="on">
         challenge
         <v-icon right>mdi-sword-cross</v-icon>
       </v-btn>
     </template>
     <v-card>
       <v-card-title>
-        <span class="headline">Game options</span>
+        <v-icon left>mdi-cog</v-icon>
+        Game options
       </v-card-title>
       <v-card-text>
-        {{ inputs }}
         <v-container>
           <v-row>
             <v-col cols="12" sm="6">
               <v-select
-                v-model="inputs.maps"
-                :items="['1', '2', '3', '4']"
-                label="Maps"
+                v-model="inputs.map"
+                :items="[1, 2, 3, 4]"
+                label="Map"
                 required
-              ></v-select>
+              />
             </v-col>
             <v-col cols="12" sm="6">
               <v-select
                 v-model="inputs.nbGames"
-                :items="['1', '2', '3', '4']"
+                :items="[1, 2, 3, 4]"
                 label="Number of games per set"
                 required
-              ></v-select>
+              />
             </v-col>
             <v-col cols="12" sm="6">
               <v-select
@@ -93,7 +87,8 @@
         </v-container>
       </v-card-text>
       <v-card-actions>
-        <v-spacer></v-spacer>
+        <v-spacer />
+        <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn>
         <v-btn color="blue darken-1" text @click="submit">OK</v-btn>
       </v-card-actions>
     </v-card>
@@ -117,16 +112,12 @@ export default class Viewer extends Vue {
 
   loading = false
   dialog = false
-  paddleVelocity: number = 1
-  ballVelocity: number = 1
-  map: number = 0
-  nbGames: number = 3
 
   inputs = {
     paddleVelocity: 1,
-    maps: 0,
+    map: 1,
     ballVelocity: 1,
-    nbGames: 3,
+    nbGames: 3, // TODO Rename
   }
 
   get small() {
@@ -137,40 +128,18 @@ export default class Viewer extends Vue {
     return this.$vuetify.breakpoint.name === 'xs'
   }
 
-  // paddleClick(data: any) {
-  //   let paddleArray = ['Slow', 'Normal', 'Fast', 'Very fast']
-  //   this.paddleVelocity = paddleArray.findIndex(
-  //     (paddleArray) => paddleArray === data
-  //   )
-  // }
-
-  // mapsClick(data: any) {
-  //   let mapsArray = ['1', '2', '3', '4']
-  //   this.map = mapsArray.findIndex((mapsArray) => mapsArray === data)
-  // }
-
-  // ballClick(data: any) {
-  //   let ballArray = ['Slow', 'Normal', 'Fast', 'Very fast']
-  //   this.ballVelocity = ballArray.findIndex((ballArray) => ballArray === data)
-  // }
-
   async submit() {
     this.dialog = false
     if (this.loading) {
       return
     }
-    console.log('INPU MAP: ' + this.inputs.maps)
+
     this.loading = true
-    //  console.log(this.inputs.map)
-    //     console.log(this.inputs.ballVelocity)
-    //     console.log(this.inputs.paddleVelocity)
+
     try {
       await this.$axios.post(`pending-games`, {
         peerId: this.peer.id,
-        map: this.inputs.maps,
-        ballVelocity: this.inputs.ballVelocity,
-        paddleVelocity: this.inputs.paddleVelocity,
-        nbGames: this.inputs.nbGames,
+        ...this.inputs,
       })
 
       this.$emit('success')
