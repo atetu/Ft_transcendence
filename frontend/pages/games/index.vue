067431aff959054fafa894021db4160fe1215079
Caceresenzo <caceresenzo1502@gmail.com>
2021-07-25 20:39:16 +0200

    chore(front/game): displaying 'socket not connected'

---
+++ frontend/pages/games/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <page-loading v-if="loading" title="Joining waiting room..." />
+  <page-error v-else-if="error" :error="error" @click="tryJoin" />
+  <page-loading v-else-if="joined" title="Waiting for opponent..." />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { Socket } from 'vue-socket.io-extended'
+
+@Component
+export default class WaitingRoom extends Vue {
+  loading = false
+  joined = false
+  error: any = null
+
+  mounted() {
+    this.tryJoin()
+  }
+
+  tryJoin() {
+    this.loading = true
+    this.error = null
+
+    this.$socket.client.emit(
+      'waiting_room_join',
+      {
+        id: 0,
+      },
+      (error: any) => {
+        if (error) {
+          this.error = error
+          console.log(error)
+        } else {
+          this.joined = true
+        }
+
+        this.loading = false
+      }
+    )
+  }
+
+  @Socket('game_starting')
+  onGameStarting(data: any) {
+    console.log({ data })
+    const { player1, player2, id } = data
+    this.joined = true
+    this.$router.replace({ path: `/games/${id}` })
+  }
+
+  destroyed() {
+    if (!this.joined) {
+      this.$socket.client.emit('waiting_room_leave', {
+        id: 0,
+      })
+    }
+  }
+}
+</script>
